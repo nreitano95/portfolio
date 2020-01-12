93a91651f9a8ba73f71ed917b78d739e960df2c2
Nick Reitano <nreitano95@gmail.com>
2020-01-11 17:58:34 -0700

    prod update

---
+++ src/Components/under_dev/under_dev.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+
+const under_dev = () => {
+    return (
+      <header id="home">
+         <div className="banner">
+            <div className="banner-text">
+               <h1 className="grow dib">Hi, I am Nick Reitano</h1><br/>
+               <h2 className="grow dib">Portfolio Under Development</h2><hr></hr>
+            </div>
+         </div>
+      </header>
+    );
+  }
+
+export default under_dev;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,19 +1,21 @@
 
 import React, { Component } from 'react';
-import Header from '../Components/Header/Header';
-import About from '../Components/About/About';
-import Resume from '../Components/Resume/Resume';
-import Portfolio from '../Components/Portfolio/Portfolio';
-import Contact from '../Components/Contact/Contact';
-import Footer from '../Components/Footer/Footer';
+// import Header from '../Components/Header/Header';
+// import About from '../Components/About/About';
+// import Resume from '../Components/Resume/Resume';
+// import Portfolio from '../Components/Portfolio/Portfolio';
+// import Contact from '../Components/Contact/Contact';
+// import Footer from '../Components/Footer/Footer';
+import Under_Dev from '../Components/under_dev/under_dev';
 
 class App extends Component {
 
   render() {
     return (
       <div className='App'>
-        <Header />
-        {/* <Portfolio />
+        <Under_Dev />
+        {/* <Header />
+        <Portfolio />
         <Resume />        
         <About />
         <Contact />
