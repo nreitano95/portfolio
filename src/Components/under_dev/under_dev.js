ac954f21cd1b7b5fce42ef969a20203fec0b435f
Nick Reitano <nreitano95@gmail.com>
2020-01-13 13:48:40 -0700

    added linkedin bug

--- src/Components/under_dev/under_dev.js
+++ src/Components/under_dev/under_dev.js
@@ -10,6 +10,11 @@ const under_dev = () => {
                <h1 className="grow dib">Hello, World!</h1><br/>
                <h2 className="grow dib">Portfolio Under Development</h2><hr></hr>
             </div>
+            <div className="grow dib">
+               <a href="https://www.linkedin.com/in/nickreitano/">
+                  <img src="LI-In-Bug.png" width="40px" height="30px"></img>
+               </a>
+            </div>
          </div>
       </header>
     );
