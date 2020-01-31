05db88aca8337ca44bbd39533b7696e5fab55769
Nick Reitano <nreitano95@gmail.com>
2020-01-30 22:24:34 -0700

    Added react-router-dom and made homepage

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,24 +1,20 @@
-
 import React, { Component } from 'react';
-import Header from '../Components/Header/Header';
-import About from '../Components/About/About';
-import Resume from '../Components/Resume/Resume';
-import Portfolio from '../Components/Portfolio/Portfolio';
-import Contact from '../Components/Contact/Contact';
-import Footer from '../Components/Footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import HomePage from '../../src/Pages/HomePage/homepage.js';
 
 class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <Header />
-        <Portfolio />
-        <Resume />        
-        <About />
-        <Contact />
-        <Footer />
-      </div>
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            {/* <Route path='/shop' component={ShopPage} /> */}
+          </Switch>
+        </div>
+      </Router>
     );
   };
 }
